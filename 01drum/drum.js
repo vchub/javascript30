dea8f289aa1d6580333d79242691312fcef6a7bd
vchub <vlad.chub@gmail.com>
2022-04-18 22:37:57 -0700

    add ./sounds, make keydown event

--- 01drum/drum.js
+++ 01drum/drum.js
@@ -1,4 +1,4 @@
-function drum(event) {
+function drumClick(event) {
   // console.log('e', event);
   const btn = event.target;
   btn.className = 'play';
@@ -15,6 +15,19 @@ function drum(event) {
 // add listner for every button
 const buttons = document.querySelectorAll('#keyboard > button');
 for (const b of buttons) {
-  b.addEventListener('click', drum);
-  // b.onclick = drum;
+  b.addEventListener('click', drumClick);
 }
+
+// collect all audio and their keys
+const aa = [...document.querySelectorAll('audio')];
+const audioMap = aa.reduce((acc, x) => {
+  acc[x.dataset.key] = x;
+  return acc;
+}, {});
+
+// add listner for keys
+window.addEventListener('keydown', (event) => {
+  if (event.key in audioMap) {
+    audioMap[event.key].play();
+  }
+});
